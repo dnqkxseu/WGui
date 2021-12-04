0248f6a74b5bbc73dc3d1a5b9bb24ac8db7c93c2
dnqkxseu <dnqkxseu@criptext.com>
2021-12-04 18:27:39 +0300

    Project updated

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using SharpWebview;
+using SharpWebview.Content;
 
 namespace WGui
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var w = new Webview())
+            {
+                w.SetTitle("WebView");
+                w.SetSize(800, 600, WebviewHint.None);
+                w.Navigate(new UrlContent("https://html.duckduckgo.com"));
+                w.Run();
+            }
         }
     }
 }
